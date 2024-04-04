bcd39e7ed06dbc87e9eb59cf34f4ebefc6e43547
DULGUUN1210 <165978948+DULGUUN1210@users.noreply.github.com>
2024-04-04 11:49:52 +0800

    file

---
+++ app.js
@@ -0,0 +1,72 @@
+var a=20, b=5, c=15, d=3;
+if(a>=10){
+	a=a;
+}else{
+	a=0;
+}
+if(b>=10){
+	b=b;
+}else{
+	b=0;
+}
+if(c>=10){
+	c=c;
+}else{
+	c=0;
+}
+if(d>=10){
+	d=d;
+}else{
+	d=0;
+}
+console.log(a+b+c+d);	
+var a=2, b=15, c=3, d=55;
+if(a<=10){
+	a=a;
+}else{
+	a=1;
+}
+if(b<=10){
+	b=b;
+}else{
+	b=1;
+}
+if(c<=10){
+	c=c;
+}else{
+	c=1;
+}
+if(d<=10){
+	d=d;
+}else{
+	d=1;
+}
+console.log(a*b*c*d);
+var too1=2, too2=15, too3=5,too4=55;
+var count=0;
+if(too1==5){
+	count++;
+}	
+if(too2==5){
+	count++;
+}
+if(too3==5){
+	count++;
+}
+if(too4==5){
+	count++;
+}
+console.log("count:"+count);
+alert("ta bolx friut end xolbogloo")
+alert("ta buirt sead torloo ta lv 1 geec 10 lv xurex octou  ta tag xeen tuld combat darg 5 lv tee huneeg alax ouctou")
+alert("ta 10 lv xurlee jugul arluu auvnaa uu")
+var  songolt1=prompt("jims/catana")
+if(songolt1=="jims"){
+	alert("ta jimseeg cong vol ulm hugtee boloo")
+	alert("ta 10 lv haaneeg alaj  hadlaa")
+}else if("songolt1==catana"){
+		alert("ta catana songvol tal hugtee boloo")
+	alert("ta 10 lv haaneeg alaj hadahguu uxlee")
+}else  {
+	alert("ta boss eeg alsan tul seacod sea oroh boloo")
+}		
